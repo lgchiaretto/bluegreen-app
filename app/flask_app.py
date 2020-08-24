a7f5396d5dd55cbeb8199412e56e33b23c8b306e
Luiz Chiaretto <luizgustavo@chiaretto.com.br>
2020-08-24 15:35:46 -0300

    first commit

---
+++ app/flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import time
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def print_message():
+    return = "I am running on '" + os.environ['APP'] + "' app!!!"
+
+@app.route('/healthcheck', methods=['GET'])
+def print_helthcheck():
+    return "ok"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
